679cf812a66ceb8a4a0be621e0332939e60547f2
alwan <mardika200@gmail.com>
2024-07-09 11:23:21 +0700

    Php Data Objects (PDO)

---
+++ koneksi.php
@@ -0,0 +1,17 @@
+<?php
+
+$dsn = 'mysql:dbname=dbbarang;host=localhost';
+$user = 'root';
+$password = '';
+
+
+try {
+    $dbh = new PDO($dsn, $user, $password); // PDO Driver DSN. Throws A PDOException.
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dbh->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, TRUE);
+    echo 'Alhamdullilah Sukses';
+} catch (PDOException $Exception) {
+    // PHP Fatal Error. Second Argument Has To Be An Integer, But PDOException::getCode Returns A
+    // String.
+    echo 'Gagal Koneksi DB karena ' . $Exception->getMessage();
+}
